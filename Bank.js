7644b43cb9b91fc99d36d97aa569fbc923b2ca05
Khin-KHTO <khin.hto79@gmail.com>
2025-01-22 20:53:58 +0100

    brackets removed

--- Bank.js
+++ Bank.js
@@ -25,35 +25,51 @@ class Account {
   // Add methods here:
   // Example: deposit(amount)
   // example data to be stored in transactionHistory { transactionType: 'Deposit', amount: 500 }
-deposit(amount) {
-return;
-}
-this.balance += amount;
-this.transactionHistory.push([{ transactionType: 'Deposit', amount: 500 }])
-  
+  deposit(amount) {
+    this.balance += amount;
+    this.transactionHistory.push({
+      transactionType: "Deposit",
+      amount,
+    });
+  }
+
   // Example: withdraw(amount)
   // example data to be stored in transactionHistory { transactionType: 'Withdrawal', amount: 200 }
-withdraw(amount) {
-  return;
-}
-this.balance -= amount;
-this.transactionHistory.push({transactionType: 'Withdrawal', amount: 200 })
-
-
+  withdraw(amount) {
+    if (amount <= this.balance) {
+      this.balance -= amount;
+      this.transactionHistory.push({
+        transactionType: "Withdrawal",
+        amount,
+      });
+    }
+  }
   // Example: transfer(amount, recipientAccount)
   // example data to be stored in transactionHistory:
   // for account sending { transactionType: 'Transfer', amount: 300, to: recipientName }
   // for account recieving { transactionType: 'Received', amount: 300, from: senderName }
 
-transfer(amount, recipientAccount){
-  return;
-}
+  transfer(amount, recipientAccount) {
+    if (amount <= this.balance) {
+      this.balance -= amount;
+      recipientAccount.balance += amount;
+      this.transactionHistory.push({
+        transactionType: "Transfer",
+        amount,
+        to: recipientAccount.name,
+      });
 
-
+      recipientAccount.transactionHistory.push({
+        transactionType: "Received",
+        amount,
+        from: this.name,
+      });
+    }
+  }
   // Example: checkBalance()
-
-
-
+  checkBalance() {
+    return this.balance;
+  }
 }
 
 //<-------------------------------DO NOT WRITE BELOW THIS LINE------------------------------>
